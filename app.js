2783599162dec667947740e1be434fee2effc34b
Rafael Pereira <51100541+rafsp@users.noreply.github.com>
2025-06-02 22:28:59 -0300

    first commit

--- app.js
+++ app.js
@@ -33,6 +33,60 @@ app.get('/produtos', (req, res) => {
   res.status(200).json(produtos);
 });
 
+app.put('/produtos/:id', (req, res) => {
+  const { nome, preco } = req.body;
+  const index = produtos.findIndex(p => p.id === req.params.id);
+
+  if (index === -1) {
+    return res.status(404).json({ erro: "Produto não encontrado" });
+  }
+
+  if (!nome || preco == null || preco <= 0) {
+    return res.status(400).json({ erro: "Nome e preço válidos são obrigatórios" });
+  }
+
+  produtos[index] = { ...produtos[index], nome, preco };
+  res.status(200).json(produtos[index]);
+});
+
+app.patch('/produtos/:id', (req, res) => {
+  const produto = produtos.find(p => p.id === req.params.id);
+  if (!produto) {
+    return res.status(404).json({ erro: "Produto não encontrado" });
+  }
+
+  Object.assign(produto, req.body);
+  res.status(200).json(produto);
+});
+
+app.delete('/produtos/:id', (req, res) => {
+  const index = produtos.findIndex(p => p.id === req.params.id);
+  if (index === -1) {
+    return res.status(404).json({ erro: "Produto não encontrado" });
+  }
+
+  produtos.splice(index, 1);
+  res.status(204).send();
+});
+
+
+
+app.get('/produtos', (req, res) => {
+  const { nome } = req.query;
+  let lista = produtos;
+
+  if (nome) {
+    lista = produtos.filter(produto =>
+      produto.nome.toLowerCase().includes(nome.toLowerCase())
+    );
+  }
+
+  if (lista.length === 0) {
+    return res.status(204).send();
+  }
+
+  return res.status(200).json(lista);
+});
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
